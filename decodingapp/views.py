acfdcb9c4d9455dac5dc05fe63db243f2858c0a9
abdurasulx <abdurasulx1@gmail.com>
2025-03-14 01:51:05 +0500

    second

--- decodingapp/views.py
+++ decodingapp/views.py
@@ -8,5 +8,27 @@ def home(request):
         print(src[i],'kuku')
     return render(request,'index.html',{'login':'banda','pass':'bandacap','ques':svl})
 def seeque(request,slug):
-    return 1
+    ans=request.POST.get('answer')
+    src=''
+    try: 
+        src=Savol.objects.get(id=slug)
+    except:
+        src=''
+
+    
+    if src!='':
+        if not ans:
+            ans=''
+            return render(request,'post.html',{'login':'banda','pass':'bandacap','savol':src,'a':ans,'correct':'1'})
+        else:
+            if ans==src.answer:
+                return render(request,'post.html',{'login':'banda','pass':'bandacap','savol':src,'a':ans,'correct':'2'})
+            if ans!=src.answer:
+                return render(request,'post.html',{'login':'banda','pass':'bandacap','savol':src,'a':ans,'correct':'3'})
+
+
+        
+        
+    else:
+        return render(request,'404.html')
 # Create your views here.
