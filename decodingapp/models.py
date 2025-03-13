29f694b6bbe1e4b448aca047eca1032ba5c4acbc
abdurasulx <abdurasulx1@gmail.com>
2025-03-13 21:55:50 +0500

    first commit

--- decodingapp/models.py
+++ decodingapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Savol(models.Model):
+    savol=models.TextField(max_length=255,blank=False)
+    javob=models.TextField(max_length=255,blank=False)
+    def __str__(self):
+        return  f'{self.savol}'
+class Usr(models.Model):
+    log=models.TextField(max_length=255,blank=False,null=False)
+    pas=models.TextField(max_length=255,blank=False,null=False)
+    lvl=models.IntegerField(default=1)
 
 # Create your models here.
--- decodingapp/views.py
+++ decodingapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def home(request):
+    src=request.POST
+    return render(request,'index.html',{'login':'banda','pass':'bandacap'})
 # Create your views here.
